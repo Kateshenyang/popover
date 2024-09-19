2f6dceaf6b438875da0d37173d7123993d93ac24
Ekaterina <kateshenyang@gmail.com>
2024-09-19 20:55:29 +0300

    updated

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 9000;
 
+// Указываем папку dist как статическую
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Запуск сервера
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
